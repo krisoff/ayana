2de9b9a03a0ab26b9cd5eda66bf414a51814f893
krisoff <przysiezniakkrystian@gmail.com>
2019-12-31 19:19:14 +0100

    kontakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ManuComponent } from './manu/manu.component';
 import { AboutComponent } from './about/about.component';
 import { CostTableComponent } from './cost-table/cost-table.component';
 import {AppRouting} from './app.routing';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ManuComponent,
     AboutComponent,
-    CostTableComponent
+    CostTableComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,15 @@ import {Injectable, NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CostTableComponent} from './cost-table/cost-table.component';
 import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
 
 const appRoutes: Routes = [
   {path: 'home',
   component: AboutComponent},
   {path: 'costs',
-  component: CostTableComponent}
+  component: CostTableComponent},
+  {path: 'contact',
+    component: ContactComponent}
 ];
 
 @Injectable()
